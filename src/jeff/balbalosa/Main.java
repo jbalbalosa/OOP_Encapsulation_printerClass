9f14f771dbad1e27aacfebcbb238ee825fcae9d6
Jeffrey Balbalosa <31784615+jbalbalosa@users.noreply.github.com>
2022-12-21 09:43:05 +0900

    commit

--- src/jeff/balbalosa/Main.java
+++ src/jeff/balbalosa/Main.java
@@ -1,5 +1,23 @@
+package jeff.balbalosa;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Udemy: Java Programming Masterclass");
+        System.out.println("OOP Encapsulation");
+        System.out.println("");
+        System.out.println("");
+
+        Printer printer = new Printer(100, 0, true);
+
+        printer.print(571);
+
+        System.out.println("Toner level: %" + printer.getTonerLevel());
+        System.out.println("Number of pages printed: " + printer.getPagePrinted());
+        System.out.println("Duplex: " + printer.getDuplexPrinter());
+
+        System.out.println("Refill");
+        printer.refill(25);
+        System.out.println("Toner level: %" + printer.getTonerLevel());
+
     }
 }
--- src/jeff/balbalosa/Printer.java
+++ src/jeff/balbalosa/Printer.java
@@ -1,2 +1,42 @@
-package jeff.balbalosa;public class Printer {
+package jeff.balbalosa;
+
+public class Printer {
+    private double tonerLevel;
+    private int pagePrinted;
+    private boolean duplexPrinter = true;
+
+    public Printer(int ink, int page, boolean duplex) {
+        this.tonerLevel = ink;
+        if (ink <= 0){
+            System.out.println("Please refill the toner");
+        }
+        this.pagePrinted = page;
+        this.duplexPrinter = duplex;
+    }
+
+    public void refill(int level){
+        this.tonerLevel += level;
+        if ((this.tonerLevel) > 100) {
+            this.tonerLevel = 100;
+        }
+    }
+
+    public void print(int pages){
+        if (this.tonerLevel > 0 ){
+            this.pagePrinted += pages;
+            this.tonerLevel -= (pages / 100);
+        }
+    }
+
+    public double getTonerLevel() {
+        return tonerLevel;
+    }
+
+    public int getPagePrinted() {
+        return pagePrinted;
+    }
+
+    public boolean getDuplexPrinter() {
+        return duplexPrinter;
+    }
 }
